35a8ae51f493122f8e1c2b0383c8ee2803eeba0f
ongood <lxd@ongood.cn>
2018-11-22 09:24:26 +0800

    add 锐浪报表

---
+++ print_rubylong/__manifest__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': u"OnGood 锐浪报表 基础模块",
+    'summary': u'CAP',
+    'version': '1.0',
+    'category': u'rubylong',
+    'sequence': 0,
+    'author': u'OnGood,Amos',
+    'website': u'https://www.ongood.cn',
+    'depends': [],
+    'version': '0.2',
+    'data': [
+       
+    ],
+    'qweb': [
+        'static/src/xml/report.xml',
+    ],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'description': """
+CAP锐浪报表输出
+""",
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
